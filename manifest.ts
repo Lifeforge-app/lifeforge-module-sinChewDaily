8a1f8dd7396ac8ee79fa8dd9857a4e4ec0f87327
Melvin Chia <melvinchia623600@gmail.com>
2026-01-05 11:59:41 +0800

    feat: update to latest version from forgistry

--- manifest.ts
+++ manifest.ts
@@ -2,10 +2,7 @@ import { lazy } from 'react'
 import type { ModuleConfig } from 'shared'
 
 export default {
-  name: 'Sin Chew Daily',
-  icon: 'tabler:news',
   routes: {
     '/': lazy(() => import('@'))
   },
-  category: 'Information'
 } satisfies ModuleConfig
